cb2d44e1c728d0cf743ce8d36bc3ecbd7744bf62
yashdholariya234 <ydholariya825@rku.ac.in>
2021-01-26 12:05:04 +0530

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./route');
+
+mongoose.connect("mongodb+srv://Yash:yash123@cluster0.fl0lc.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+()=>{
+    const app = express();
+    app.use("/api",route);
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log("server started..!!");
+    })
+
+}
+);
