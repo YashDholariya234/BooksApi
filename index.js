6ae43f3a68fea4c79b93e1561437d91602c5015f
yashdholariya234 <ydholariya825@rku.ac.in>
2021-01-27 13:45:08 +0530

    fetching completed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ mongoose.connect("mongodb+srv://Yash:yash123@cluster0.fl0lc.mongodb.net/students
 ()=>{
     const app = express();
     app.use("/api",route);
-    app.listen(3000,()=>{
+    app.listen(process.env.PORT || 3000,()=>{
         console.log("server started..!!");
     })
 
