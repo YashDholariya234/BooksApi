1138483e5fe95ca730ba8f35d11ff7f4167f6b7a
yashdholariya234 <ydholariya825@rku.ac.in>
2021-01-28 10:57:08 +0530

    Done

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 
-mongoose.connect("mongodb+srv://Yash:yash123@cluster0.fl0lc.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://Yash:yash123@cluster0.fl0lc.mongodb.net/students?retryWrites=true&w=majoritymongodb+srv://Yash:yash123@cluster0.fl0lc.mongodb.net/students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+    mongoose.connect("",{useNewUrlParser:true,useUnifiedTopology:true}).then(
 ()=>{
     const app = express();
     app.use("/api",route);
